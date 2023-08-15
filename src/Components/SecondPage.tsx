6018db494c6763777b18a135c12e43a5b25365e8
Abhi-4793 <abhinavkrishnapy2@gmail.com>
2023-08-15 16:29:29 +0530

    first commit

---
+++ src/Components/SecondPage.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+const SecondPage = () => {
+  return (
+    <div className="flex flex-row w-[51.813rem] h-[291rem] bg-white ">
+      <div className="mt-[10rem] ml-[7rem] ">
+        <img
+          src="images/rectangle-3.png"
+          alt="vector"
+          className="w-[29.6875rem] h-[33.5675rem]"
+        />
+      </div>
+      <div className="w-[21.6875rem] h-[33.5675rem] pl-20">
+        <h2 className="text-[#002060] mt-[10.5rem] ml-[7rem] l text-[2.2rem] text-left font-bold w-[30.125rem] tracking-wider h-[5.0625rem ">
+          Here's what you get as a MasterMaths Student :
+        </h2>
+        <ul className="ml-[9rem] w-[30rem] text-left leading-10 font-semibold list-outside text-[#000000] flex flex-col ">
+          <li>
+            90-minutes of weekly Maths Coaching (Get mentored by qualified Maths
+            coaches)
+          </li>
+          <li>
+            Lesson Recording for All Classes (Unlimited Revision and No More
+            Forgetting What's on the Whiteboard)
+          </li>
+          <li>
+            Access to Online Maths Tutor (Ask Questions and Clear your Doubts
+            Anytime)
+          </li>
+          <li>
+            Extra Complimentary Sessions for P5/P6 and "O" Level Students (Added
+            Exams' Preparations Support)
+          </li>
+          <li>
+            {" "}
+            Feedback and Progress Updates (Get Updates on Lesson Summaries and
+            Child's Areas for Improvements)
+          </li>
+        </ul>
+        <div className="flex-col  absolute mt-[-24rem] ml-[6rem]  ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            className="mb-14"
+          >
+            <circle cx="9" cy="9" r="8.5" stroke="#F24E3D" />
+            <circle cx="9.00003" cy="8.99991" r="4.90909" fill="#F24E3D" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            className="mb-[4.1rem]"
+          >
+            <circle cx="9" cy="9" r="8.5" stroke="#F24E3D" />
+            <circle cx="9.00003" cy="8.99991" r="4.90909" fill="#F24E3D" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            className="mb-[4.0rem]"
+          >
+            <circle cx="9" cy="9" r="8.5" stroke="#F24E3D" />
+            <circle cx="9.00003" cy="8.99991" r="4.90909" fill="#F24E3D" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            className="mb-14"
+          >
+            <circle cx="9" cy="9" r="8.5" stroke="#F24E3D" />
+            <circle cx="9.00003" cy="8.99991" r="4.90909" fill="#F24E3D" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            className="mb-14"
+          >
+            <circle cx="9" cy="9" r="8.5" stroke="#F24E3D" />
+            <circle cx="9.00003" cy="8.99991" r="4.90909" fill="#F24E3D" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondPage;
---
+++ src/Components/ThirdPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ThirdPage() {
+  return <div></div>;
+}
+
+export default ThirdPage;
